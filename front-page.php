d3a67f120ebb8cfae2f4c25a1a5f9b936474c44b
Aleksandr <algryshko@gmail.com>
2024-12-25 14:12:24 +0200

    setting up popup

--- front-page.php
+++ front-page.php
@@ -2,9 +2,12 @@
 <main class="wrapper home_page"><span class="opacity-bg"></span>
 
     <section class="container ">
+        <a href="#slider-popup" class="open-popup-link">Open Popup</a>
 
-<!--        New Block-->
-
+        <div id="slider-popup" class="slider-popup mfp-hide">
+            <h2 class="popup-title">Popup Content</h2>
+            <div class="popup-content">This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. This is a simple popup. Click anywhere else on the page to close it. </div>
+        </div>
     </section>
 
 </main>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 jQuery(document).ready(function ($) {
+  $('.open-popup-link').magnificPopup({
+    type: 'inline', // Use inline content
+    midClick: true, // Allow opening the popup on middle click
+    removalDelay: 300, // Delay before removing the popup
+    mainClass: 'mfp-fade' // Fade-in/out effect
+  });
 
-
-
+  const swiper = new Swiper('.swiper-container', {
+    loop: true, // Enables infinite loop
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Enable clicking to navigate
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000, // Autoplay slides every 5 seconds
+    },
+    slidesPerView: 1, // Show one slide at a time
+    spaceBetween: 10, // Space between slides
+  });
 });
